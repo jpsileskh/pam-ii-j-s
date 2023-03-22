7c64622eb3bdf8cb2b3d5e2cb336da6a83855fa4
jpsileskh <jpsileskh@hotmail.com>
2023-03-22 08:28:42 -0300

    Aula sobre variáveis e tipos

--- aula02/funcoes.js
+++ aula02/funcoes.js
@@ -1,20 +1,65 @@
-let num1 = 2
-const num2 = 3
+// let num1 = 2
+// const num2 = 3
+// var msg = "Olá amigos! Esse é o número:"
 
-function soma() {
-    return num1 + num2
-}
+// console.log(msg + num1)
+// console.log(num1 + num2)
+// console.log(typeof num1)
+// console.log(typeof msg)
 
-console.log(soma())
+// JSON - JavaScript Object Notation
 
-num1 = 6
-// num2 = 9
+// let usuario = {
+//     nome: "João",
+//     idade: 35,
+//     signo: "Escorpião",
+//     calvo: true
+// }
 
-console.log(soma())
+// console.log (typeof usuario)
+// console.log ("nome:", usuario.nome, "\n idade:", usuario.idade)
+// console.log(typeof usuario.calvo)
 
-const funcaoSoma = (a, b) => {
-    return a + b
-}
+const nomes = ["João", "Oãoj"]
 
-console.log(funcaoSoma(10, 8))
-console.log(funcaoSoma(7, 30))
+console.log(nomes)
+console.log(typeof nomes)
+console.log(nomes[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function soma() {
+//     return num1 + num2
+// }
+
+// console.log(soma())
+
+// num1 = 6
+// // num2 = 9
+
+// console.log(soma())
+
+// const funcaoSoma = (a, b) => {
+//     return a + b
+// }
+
+// console.log(funcaoSoma(10, 8))
+// console.log(funcaoSoma(7, 30))
