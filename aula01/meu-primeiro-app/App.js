6a58224297241619b2a1b12e1b9c3e0987b02498
jpsileskh <jpsileskh@hotmail.com>
2023-09-04 22:19:33 -0300

    Aula de criação do projeto

---
+++ aula01/meu-primeiro-app/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá amigos!</Text>
+      <Text>Tudo funcionando!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
