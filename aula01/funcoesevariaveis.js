b0616de5b3c937ef77e771dcfa61f6997429e96a
jpsileskh <jpsileskh@hotmail.com>
2023-10-16 22:24:59 -0300

    Primeira aula de Javascript

---
+++ aula01/funcoesevariaveis.js
@@ -0,0 +1,52 @@
+var variavel1 = "olá";
+let variavel2 = 3;
+const variavel3 = 8;
+
+console.log(
+    "Olá mundo!"
+    );
+console.log(variavel1 + "mundo");
+console.log(variavel1, "mundo");
+console.log(variavel1+ " Turminha do "+ variavel2+ "º ano")
+console.log(variavel2 + variavel3)
+console.log(typeof variavel1)
+console.log(typeof variavel2)
+variavel1 = 4
+console.log(typeof variavel1)
+const pessoa = {
+    nome: "João",
+    signo: "Escorpião",
+    idade: 35
+}
+console.log(pessoa)
+console.log(
+    "Nome:", pessoa.nome,
+    "\nIdade:", pessoa.idade
+)
+let cachorros = ["Bug", "Zeus", "Zaratrusta"]
+console.log(cachorros[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Formato JSON - JavaScript Notation Object
+// console.log(obj.nome, obj.signo)
+// console.log(typeof obj)
