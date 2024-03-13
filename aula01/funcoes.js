137494a6f997b07028ca05588dc1038071fbb717
João Siles <jpsileskh@hotmail.com>
2024-03-13 08:31:23 -0300

    Criado arquivo para conceitos de variaveis

--- aula01/funcoes.js
+++ aula01/funcoes.js
@@ -1,11 +1,14 @@
-// let num1 = 2
-// const num2 = 3
-// var msg = "Olá amigos! Esse é o número:"
+let num1 = 2
+const num2 = 3
+var msg = "Olá amigos! Esse é o número:"
 
-// console.log(msg + num1)
-// console.log(num1 + num2)
-// console.log(typeof num1)
-// console.log(typeof msg)
+console.log(msg , num1)
+console.log(num1 + num2)
+console.log(typeof num1)
+console.log(typeof msg)
+
+
+
 
 // JSON - JavaScript Object Notation
 
@@ -20,11 +23,17 @@
 // console.log ("nome:", usuario.nome, "\n idade:", usuario.idade)
 // console.log(typeof usuario.calvo)
 
-const nomes = ["João", "Oãoj"]
 
-console.log(nomes)
-console.log(typeof nomes)
-console.log(nomes[0])
+
+
+
+
+
+// const nomes = ["João", "Oãoj"]
+
+// console.log(nomes)
+// console.log(typeof nomes)
+// console.log(nomes[0])
 
 
 
