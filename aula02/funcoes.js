70473c2098a89c8fb5c86d3ae62bc018bb1a7992
jpsileskh <jpsileskh@hotmail.com>
2024-03-27 08:34:17 -0300

    Aula sobre funções em JS

---
+++ aula02/funcoes.js
@@ -0,0 +1,44 @@
+let num1 = 2
+const num2 = 3
+var msg = "Olá amigos! Esse é o número:"
+
+console.log(msg , num1)
+console.log(num1 + num2)
+console.log(typeof num1)
+console.log(typeof msg)
+//métodos são funcionalidades prontas da linguagem e que 
+//podem receber parâmetros 
+
+
+function Somar () {
+    n1 = 2
+    n2 = 5
+    console.log("O resultado é:", n1 + n2)
+} // função é algo que eu crio! mas... tudo no js
+// é função
+
+Somar() // aqui ela é executada
+Somar()
+
+function SomarComParametros (n1 , n2) {
+    let resultado = "O resultado é: " + (n1 + n2)
+    console.log(resultado) 
+}
+
+SomarComParametros(16, 30)
+SomarComParametros(70, 54)
+
+const Teste = (n) => {
+    let result = n % 2 
+    
+    if (result == 1) {
+        return "Número impar"
+    } 
+    
+    return "Número par"
+    
+}
+
+console.log (Teste(2))
+console.log (Teste(7))
+Teste(5) // essa linha não vai executar pq não tem console
